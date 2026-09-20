70dfedba1debab99a3172a752648590a556542b7
Kouzi1 <fort3994@gmail.com>
2026-09-20 05:03:07 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,12 @@
-// Replace every placeholder with the Firebase Web App configuration
-// from Firebase Console > Project settings > Your apps > Web app.
-//
-// This normal web config is not a secret. Never place a service-account
-// private key or Firebase Admin SDK credentials in this file.
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDNoBwUAWNaumAWl-RVHkuJrJWVt8SFXvo",
   authDomain: "kloz-14442.firebaseapp.com",
+  databaseURL: "https://console.firebase.google.com/project/kloz-14442/database/kloz-14442-default-rtdb/data/~2F",
   projectId: "kloz-14442",
   storageBucket: "kloz-14442.firebasestorage.app",
   messagingSenderId: "544394652807",
   appId: "1:544394652807:web:0f11281f79871c63591c22",
   measurementId: "G-L7EJ3VRBME"
 };
+
+export { firebaseConfig };
